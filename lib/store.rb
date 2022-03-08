c3201d1bd297d17fc7dc7392d676226d1b699eae
Jordan <jordan.benson4@gmail.com>
2022-03-07 18:35:17 -0800

    Create custom validator and add validations.

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  validate :apparel_validator
+  validates_length_of :name, minimum: 3, too_short: 'Pick a longer name'
+  validates_numericality_of :annual_revenue, greater_than: 0
   has_many :employees
+
+  def apparel_validator
+    if mens_apparel.nil? && womens_apparel.nil?
+      errors.add(:mens_apparel, 'Store needs to have either mens or womens apparel.')
+      errors.add(:womens_apparel, 'Store needs to have either mens or womens apparel.')
+    end
+  end
 end
